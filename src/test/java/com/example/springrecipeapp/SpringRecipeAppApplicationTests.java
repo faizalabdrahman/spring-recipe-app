0fb8a4469bdc51a2c6f930b8c6ffdd367b71d498
faizalabdrahman <faizalabdrahman@gmail.com>
2022-02-08 13:28:48 +0800

    Initial commit.

---
+++ src/test/java/com/example/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
