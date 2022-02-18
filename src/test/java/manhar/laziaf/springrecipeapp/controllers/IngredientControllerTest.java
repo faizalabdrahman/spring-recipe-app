078d46f4d46729b753b3a00ee8c7d9bea3c71dd2
faizalabdrahman <faizalabdrahman@gmail.com>
2022-02-18 09:09:43 +0800

    Added View Ingredients Functionality. Closes #27

---
+++ src/test/java/manhar/laziaf/springrecipeapp/controllers/IngredientControllerTest.java
@@ -0,0 +1,54 @@
+package manhar.laziaf.springrecipeapp.controllers;
+
+import manhar.laziaf.springrecipeapp.commands.RecipeCommand;
+import manhar.laziaf.springrecipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class IngredientControllerTest
+{
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController ingredientController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp()
+    {
+        MockitoAnnotations.openMocks(this);
+
+        ingredientController = new IngredientController(recipeService);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+    }
+
+    @Test
+    public void listIngredients() throws Exception
+    {
+        // given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        // when
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        // then
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/ingredient/list"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+}
