b2b1f1af8124401b3286163de4ad700c1ed5fcfe
faizalabdrahman <faizalabdrahman@gmail.com>
2022-02-26 19:42:24 +0800

    Displayed images from Database. Closes #35

--- src/main/java/manhar/laziaf/springrecipeapp/controllers/ImageController.java
+++ src/main/java/manhar/laziaf/springrecipeapp/controllers/ImageController.java
@@ -1,7 +1,9 @@
 package manhar.laziaf.springrecipeapp.controllers;
 
+import manhar.laziaf.springrecipeapp.commands.RecipeCommand;
 import manhar.laziaf.springrecipeapp.services.ImageService;
 import manhar.laziaf.springrecipeapp.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 @Controller
 public class ImageController
@@ -37,5 +43,24 @@ public class ImageController
         imageService.saveImageFile(Long.valueOf(recipeId), file);
 
         return "redirect:/recipe/" + recipeId + "/show";
+    }
+
+    @GetMapping("recipe/{recipeId}/recipeimage")
+    public void renderImageFromDB(@PathVariable String recipeId, HttpServletResponse response) throws Exception
+    {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+
+        byte[] byteArray = new byte[recipeCommand.getImage().length];
+
+        int i = 0;
+
+        for(Byte wrappedByte : recipeCommand.getImage())
+        {
+            byteArray[i++] = wrappedByte;
+        }
+
+        response.setContentType("image/jpeg");
+        InputStream inputStream = new ByteArrayInputStream(byteArray);
+        IOUtils.copy(inputStream, response.getOutputStream());
     }
 }
--- src/test/java/manhar/laziaf/springrecipeapp/controllers/ImageControllerTest.java
+++ src/test/java/manhar/laziaf/springrecipeapp/controllers/ImageControllerTest.java
@@ -7,15 +7,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class ImageControllerTest
@@ -69,5 +70,36 @@ public class ImageControllerTest
                 .andExpect(view().name("redirect:/recipe/1/show"));
 
         verify(imageService, times(1)).saveImageFile(anyLong(), any());
+    }
+
+    @Test
+    public void renderImageFromDB() throws Exception
+    {
+        // given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        String fakeImage = "this is an image";
+        Byte[] bytesBoxed = new Byte[fakeImage.getBytes().length];
+
+        int i = 0;
+
+        for(byte primByte : fakeImage.getBytes())
+        {
+            bytesBoxed[i++] = primByte;
+        }
+
+        recipeCommand.setImage(bytesBoxed);
+
+        // when
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeimage"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+
+        byte[] responseBytes = response.getContentAsByteArray();
+
+        assertEquals(fakeImage.getBytes().length, responseBytes.length);
     }
 }
